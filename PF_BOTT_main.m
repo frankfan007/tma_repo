1564d992f5a35cfd2f2c71289ea7c5861b3bdb53
hsnhsyn1 <hsnhsyn13@gmail.com>
2019-03-18 12:47:01 +0300

    fena değil

--- PF_BOTT_main.m
+++ PF_BOTT_main.m
@@ -5,7 +5,7 @@
 clc; clearvars; close all;
 
 % rng('default')
-MC = 100;           % number of Monte Carlo runs
+MC = 1;           % number of Monte Carlo runs
 
 for mc = 1:MC
     mc
@@ -15,7 +15,7 @@ for mc = 1:MC
     
     %%  particle, weight, state initialization
     zk_1        = Measures(mc).Z{1};                % first measurement
-    m_init      = [7000; -4; 1000; -4];      % particle initialization state
+    m_init      = [7000; -2; 1000; -2];      % particle initialization state
     own         = GTruth.Ownship(:,1);
     
     Xki     = initParticles(m_init, model.P_init, own, model.N, model);     % initial particles
@@ -26,7 +26,7 @@ for mc = 1:MC
     Result(mc).P{1} = Xki*Xki'./model.N;            % initial state covariance
     
     zvec(:,1) = zk_1;
-    for k = 1:model.K       % total number of scans
+    for k = 2:model.K       % total number of scans
         %     refresh;
         zk = Measures(mc).Z{k};                         % current measurement
         zvec(:,k) = zk;
--- SampleWeights.m
+++ SampleWeights.m
@@ -10,7 +10,7 @@
 
 function Wki = SampleWeights(xki, wki_prev, zk, own, model)
 
-Likelihood  = computeLikelihood(zk, xki, own, model)'+eps;        % likelihood value as the predicted weight
+Likelihood  = computeLikelihood(zk, xki, own, model)';        % likelihood value as the predicted weight
 Wki         = Likelihood.*wki_prev;
 % Wki         = Wki_tilde/sum(Wki_tilde);                     % normalized weights
 
